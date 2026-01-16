8d88eba8da05496a2b2d5c3daf765b7a94285871
Itoro Akpan <iakpan6@byupathway.edu>
2026-01-16 20:13:10 +0100

    week1 assignment completed

---
+++ scripts/getdates.js
@@ -0,0 +1,13 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // JavaScript code
+});
+
+// output the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// output the last modified date
+document.getElementById("lastModified").innerHTML =
+    "Last Modified: " + document.lastModified;
