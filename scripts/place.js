2d2fe99a1e13bf87aa5c2e983ad64589143e16bb
Itoro Akpan <iakpan6@byupathway.edu>
2026-03-11 20:56:42 +0100

    update place.html

--- scripts/place.js
+++ scripts/place.js
@@ -1,10 +1,8 @@
 // Display current year
-const year = document.querySelector("#year");
-year.textContent = new Date().getFullYear();
+document.querySelector("#year").textContent = new Date().getFullYear();
 
 // Display last modified date
-const lastModified = document.querySelector("#lastModified");
-lastModified.textContent = document.lastModified;
+document.querySelector("#lastModified").textContent = document.lastModified;
 
 // Wind Chill Calculation
 const temp = parseFloat(document.querySelector("#temp").textContent);
@@ -12,15 +10,9 @@ const wind = parseFloat(document.querySelector("#wind").textContent);
 const windChill = document.querySelector("#windchill");
 
 function calculateWindChill(t, s) {
-    return (
-        13.12 +
-        0.6215 * t -
-        11.37 * Math.pow(s, 0.16) +
-        0.3965 * t * Math.pow(s, 0.16)
-    ).toFixed(1);
+    return (13.12 + 0.6215 * t - 11.37 * Math.pow(s, 0.16) + 0.3965 * t * Math.pow(s, 0.16)).toFixed(1);
 }
 
-// Check conditions for wind chill
 if (temp <= 10 && wind > 4.8) {
     windChill.textContent = calculateWindChill(temp, wind) + " °C";
 } else {
