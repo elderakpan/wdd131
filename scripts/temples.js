bdfde5edd750c108f25e3380a0c1f267510e588a
Itoro Etim Akpan Mendie <itoroakpan@email.com>
2026-03-20 20:31:18 +0100

    temples..html updates

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,18 +1,17 @@
-// Footer year and last modified
+// Footer
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
-// Hamburger toggle
+// Hamburger menu
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
-    if (navMenu.style.display === "flex") {
-        navMenu.style.display = "none";
-        hamburger.textContent = "\u2630"; // Hamburger icon
+    navMenu.classList.toggle("hide");
+
+    if (navMenu.classList.contains("hide")) {
+        hamburger.textContent = "\u2630"; // ☰
     } else {
-        navMenu.style.display = "flex";
-        navMenu.style.flexDirection = "column";
-        hamburger.textContent = "\u2715"; // X icon
+        hamburger.textContent = "\u2715"; // ✕
     }
 });
